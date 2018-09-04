25c82b59cfcb305f5c34c330a6f7ebb7907a749b
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2018-09-04 11:15:05 +0200

    More tries for windows compile

--- src/main/c/compile_windows.sh
+++ src/main/c/compile_windows.sh
@@ -1,8 +1,39 @@
 #!/bin/sh
 CWD=$(pwd)
-CXX="g++ -std=c++11 -s -shared -fPIC -fno-builtin-memcmp -DOS_WINDOWS -DLEVELDB_PLATFORM_POSIX -DLEVELDB_ATOMIC_PRESENT -DNDEBUG -O2 -DDLLX= -pthread -I$JAVA_HOME/include/ -I$JAVA_HOME/include/win32/ -I$CWD/leveldb-mcpe/ -I$CWD/leveldb-mcpe/include/"
 
+# First we start by this: you NEED cygwin with installed x86_64-w64-mingw32-g++ for this to work
+CXX="g++"
+
+# We compile with c++0x because leveldb uses it as preferred language level
+CXX="$CXX -std=c++11"
+
+# Some shared library switches
+CXX="$CXX -s -shared -fPIC"
+
+# This build should use the libc memcmp instead of the builtin one
+CXX="$CXX -fno-builtin-memcmp"
+
+# Since we use a "emulated" windows build chain we need to define that we are windows 7
+CXX="$CXX -D_WIN32_WINNT=0x0601"
+
+# Now some leveldb config (telling we are windows)
+CXX="$CXX -DOS_WIN -DWIN32 -DNDEBUG -DDLLX="
+
+# Some general improvements (seems buggy)
+# CXX="$CXX -O2"
+
+# Includes
+CXX="$CXX -I$JAVA_HOME/include/ -I$JAVA_HOME/include/win32/ -I$CWD/leveldb-mcpe/ -I$CWD/leveldb-mcpe/include/"
+
+# We need the zlib.h in a seperate folder (because mojang added it to the wrong folder zlib/zlib.h where the normal import is simply zlib.h)
+mkdir -p "$CWD/include/zlib/"
+cp /usr/include/zlib.h "$CWD/include/zlib/zlib.h"
+CXX="$CXX -I$CWD/include/"
+
+# Verify that the built cmd is correct
 echo $CXX
+
+# Build the windows version
 $CXX leveldb-mcpe/db/builder.cc      leveldb-mcpe/db/c.cc\
      leveldb-mcpe/db/db_impl.cc      leveldb-mcpe/db/db_iter.cc\
      leveldb-mcpe/db/dbformat.cc     leveldb-mcpe/db/filename.cc\
@@ -25,12 +56,13 @@ $CXX leveldb-mcpe/db/builder.cc      leveldb-mcpe/db/c.cc\
      leveldb-mcpe/util/arena.cc      leveldb-mcpe/util/bloom.cc\
      leveldb-mcpe/util/cache.cc      leveldb-mcpe/util/coding.cc\
      leveldb-mcpe/util/comparator.cc leveldb-mcpe/util/crc32c.cc\
-     leveldb-mcpe/util/env.cc        leveldb-mcpe/util/env_posix.cc\
+     leveldb-mcpe/util/env.cc        leveldb-mcpe/util/env_win.cc\
      leveldb-mcpe/util/filter_policy.cc\
      leveldb-mcpe/util/hash.cc\
      leveldb-mcpe/util/histogram.cc  leveldb-mcpe/util/logging.cc\
-     leveldb-mcpe/util/options.cc    leveldb-mcpe/util/status.cc\
+	 leveldb-mcpe/util/win_logger.cc leveldb-mcpe/util/options.cc\
+	 leveldb-mcpe/util/status.cc\
      \
-     leveldb-mcpe/port/port_posix.cc leveldb-mcpe/port/port_posix_sse.cc\
+     leveldb-mcpe/port/port_win.cc leveldb-mcpe/port/port_posix_sse.cc\
 	 \
-	 io_gomint_leveldb_DB.cpp io_gomint_leveldb_Iterator.cpp io_gomint_leveldb_WriteBatch.cpp leveldbjni.cpp -o ../resources/leveldb-mcpe.so -lz
+	 io_gomint_leveldb_DB.cpp io_gomint_leveldb_Iterator.cpp io_gomint_leveldb_WriteBatch.cpp leveldbjni.cpp -o ../resources/leveldb-mcpe.dll -lz
---
+++ src/main/java/io/gomint/leveldb/TestRunner.java
@@ -0,0 +1,12 @@
+package io.gomint.leveldb;
+
+/**
+ * @author geNAZt
+ */
+public class TestRunner {
+
+    public static void main( String[] args ) {
+        System.load( "D:\\leveldb-mcpe.dll" );
+    }
+
+}
